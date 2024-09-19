6f9e94e258251f964b57f948b80e52776b8061f1
ivi <ivan.ariel.ialonardi@gmail.com>
2024-09-19 17:19:11 -0300

    terminando punto 10

--- ejercicios/ejercicio10.js
+++ ejercicios/ejercicio10.js
@@ -1,14 +1,16 @@
 //Crea una galería de imágenes. Al hacer clic en una imagen, muestra su descripción en un elemento `<p>`.
 export const punto10 = imgWrapperId => {
     const wrapper = document.getElementById(imgWrapperId)
-    const p = document.createElement('p')//el dentro del event listener creaba uno nuevo entonces tiraba un erro al eliminar
-    wrapper.childNodes.forEach(children => children.addEventListener('click', () => {
-        if (!children.querySelector('p')) {
-            p.textContent = children.querySelector('img').alt
-            children.appendChild(p)
-        }
-        else {
-            children.removeChild(p)
-        }
-    }))
+    wrapper.childNodes.forEach(children => {
+        const p = document.createElement('p')//dentro del event listener creaba uno nuevo entonces tiraba un error al eliminar
+        children.addEventListener('click', () => {
+            if (!children.querySelector('p')) {
+                p.textContent = children.querySelector('img').alt
+                children.appendChild(p)
+            }
+            else {
+                children.removeChild(p)
+            }
+        })
+    })
 }
