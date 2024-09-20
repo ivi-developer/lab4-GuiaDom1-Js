89296266a4c57dff1304b6039174b3ba864a8efb
ivi <ivan.ariel.ialonardi@gmail.com>
2024-09-20 09:03:54 -0300

    haciendo que ande en la facu con http-server

--- ejercicios/ejericicio14.js
+++ ejercicios/ejericicio14.js
@@ -1,1 +1,5 @@
 // Crea una tabla con filas y columnas. Al hacer clic en una celda, cambia su contenido.
+export const punto14=tablaId=>{
+    const tabla=document.getElementById(tablaId)
+    tabla.addEventListener('click',()=>{})
+}
